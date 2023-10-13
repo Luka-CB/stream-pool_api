afc2bc65cb14144083f3fafb3c194aaf85fc6e8f
luka_CB <lukaaslamazashvili20@gmail.com>
2023-10-13 13:21:58 +0400

    Some Changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "25mb" }));
 
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: ["http://localhost:8080", "https://stream-pool.vercel.app"],
     credentials: true,
   })
 );
